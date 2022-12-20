8c824bd78b0b27e403146eca4e47370bf5932ad9
SUDIP <sudipmahanta999@gmail.com>
2022-12-20 20:11:05 +0530

    initial commit

---
+++ lib/card01.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+
+import 'package:cards/cards.dart';
+import 'package:flutter/material.dart';
+
+class Card01 extends StatefulWidget {
+  const Card01({Key? key}) : super(key: key);
+
+  @override
+  State<Card01> createState() => _Card01State();
+}
+
+class _Card01State extends State<Card01> {
+  double top = 0.0;
+  double left = 0.0;
+  // double right = 150.0;
+  // double bottom = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height - (
+        MediaQuery.of(context).padding.top +
+            MediaQuery.of(context).padding.bottom
+    );
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: width * 0.86,
+          height: height * 0.60,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Stack(
+            children: [
+              Dragger(
+                // bottom: bottom,
+                // right: 10,
+                left: left,
+                top: top,
+                onPanUpdate: (details) {
+                  left = max(0, left + details.delta.dx);
+                  top = max(0, top + details.delta.dy);
+                  // bottom = max(0, details.delta.dy);
+                  // right = max(0, right + details.delta.dx);
+                    setState(() {
+                      debugPrint('Left: ${left.toString()}');
+                      // debugPrint('Bottom: ${bottom.toString()}');
+                      // debugPrint('Right: ${right.toString()}');
+                      debugPrint('Top: ${top.toString()}');
+                    });
+                },
+                child: Container(
+                  width: 110,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)
+                      ),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage('https://www.visitdubai.com/-/media/gathercontent/article/t/take-the-perfect-photo-of-burj-khalifa/media/v3/best-burj-khalifa-picture-header-2.jpg')
+                      )
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/cards.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class Dragger extends StatefulWidget {
+  final double? left;
+  final double? top;
+  final double? right;
+  final double? bottom;
+  final double? width;
+  final double? height;
+  final Widget child;
+  void Function(DragUpdateDetails)? onPanUpdate;
+  Dragger({super.key, this.left, this.top, this.right, this.bottom, required this.child, this.width, this.height, this.onPanUpdate}) :
+        assert(left == null || right == null || width == null),
+        assert(right == null || bottom == null || height == null);
+
+
+  @override
+  State<Dragger> createState() => _DraggerState();
+}
+
+class _DraggerState extends State<Dragger> {
+
+  double? left;
+  double? top;
+  double? bottom;
+  double? right;
+
+  @override
+  void initState() {
+    super.initState();
+    left = widget.left;
+    top = widget.top;
+    right = widget.right;
+    bottom = widget.bottom;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Positioned(
+      right: right,
+      bottom: bottom,
+      left: left,
+      top: top,
+      width: widget.width,
+      height: widget.height,
+      child: GestureDetector(
+          onPanUpdate: widget.onPanUpdate,
+          //     (details) {
+          //   left = max(0, left! + details.delta.dx);
+          //   top = max(0, top! + details.delta.dy);
+          //   bottom = max(details.delta.dy, 0);
+          //   right = max(0, right! + details.delta.dx);
+          //   bottom = max(0, bottom! + details.delta.dy);
+          //   setState(() {
+          //     debugPrint('Left: ${left.toString()}');
+          //     debugPrint('Bottom: ${bottom.toString()}');
+          //     debugPrint('Right: ${right.toString()}');
+          //     debugPrint('Top: ${top.toString()}');
+          //   });
+          // },
+          onTap: () {
+          },
+          child: widget.child
+      ),
+    );
+  }
+}
+
